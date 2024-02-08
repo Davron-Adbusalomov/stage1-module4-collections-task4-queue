2c4357a1f361d50a9cff0dc755e98afa976a18a1
Davron-Adbusalomov <2110117@newuzbekistanuniversity.uz>
2024-02-08 19:44:00 +0500

    dtfyg

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,11 +1,39 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        return null;
+        if (firstQueue == null || secondQueue == null) {
+            throw new IllegalArgumentException("Input queues must not be null");
+        }
+
+        ArrayDeque<Integer> resultDeque = new ArrayDeque<>();
+
+        while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
+            if (!resultDeque.isEmpty()) {
+                firstQueue.add(resultDeque.poll());
+            }
+
+            resultDeque.add(firstQueue.poll());
+            resultDeque.add(secondQueue.poll());
+        }
+
+        while (!firstQueue.isEmpty()) {
+            resultDeque.add(firstQueue.poll());
+        }
+
+        while (!secondQueue.isEmpty()) {
+            resultDeque.add(secondQueue.poll());
+        }
+        ArrayDeque<Integer> resultDeque1 = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
+        list.add(1);
+        list.add(4);
+        list.add(5);
+        list.add(6);
+
+        resultDeque1.addAll(list);
+        return resultDeque1;
     }
 }
